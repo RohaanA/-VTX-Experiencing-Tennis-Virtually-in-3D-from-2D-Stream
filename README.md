1959bfdc21b20be357193ed5cd771a1d59202c2c
John Doe <johndoe@email.com>
2023-10-13 11:19:03 +0500

    Updated keypoint extracter

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # (VTX) Experiencing Tennis Virtually in 3D from 2D Stream
 
+
+
 Experience your favorite matches in 3 dimensions!
--- utils/extract_keypoints.py
+++ utils/extract_keypoints.py
@@ -1,8 +1,7 @@
 import os
 
-#Generate a function called extract_keypoints
-def extract_keypoints(results, output_directory):
-    frameCount = -1
+#This function extracts and saves the keypoints from the YOLOv8 model into a text file
+def extract_keypoints(frameCount, results, output_directory):
     # Store keypoints as JSON in a text file
     keypoints_path = os.path.join(output_directory, f"frame{frameCount}.txt")
     with open(keypoints_path, 'w') as file:
